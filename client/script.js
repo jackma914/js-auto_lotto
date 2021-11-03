01cb1ed77b1ea8a9d33d8422b3fdd23ebacbd20c
joon <jackma914@gmail.com>
2021-11-03 19:21:04 +0900

    21-11-03
    구글맵을 이용하여 로또 판매점 지도 검색 기능 구현

--- client/script.js
+++ client/script.js
@@ -42,7 +42,6 @@ class App {
         <span>${dataN.drwtNo5}</span>
         <span>${dataN.drwtNo6}</span>
       <div class="bnus-num">${dataN.bnusNo}</div>
-      <span>${dataN.drwNo}</span>
       </div>
           `;
         arrow.insertAdjacentHTML("afterend", html);
@@ -123,10 +122,94 @@ class MakeNumber {
 
 class Map {
   constructor() {
-    this._getMap();
+    // this._getMap();
+    this._getPosition();
   }
 
-  _getMap() {}
+  //geolocationd을 이용하여 위치정보를 가저왔습니다.
+  _getPosition() {
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._getMap.bind(this),
+        function () {
+          alert("Could not get your position");
+        }
+      );
+  }
+
+  _getMap(position) {
+    // navigator.geolocation.getCurrentPosition();
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: latitude, lng: longitude },
+      zoom: 13,
+      mapTypeId: "roadmap",
+    });
+    // Create the search box and link it to the UI element.
+    const input = document.getElementById("pac-input");
+    const searchBox = new google.maps.places.SearchBox(input);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener("bounds_changed", () => {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    let markers = [];
+
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener("places_changed", () => {
+      const places = searchBox.getPlaces();
+
+      if (places.length == 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach((marker) => {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      const bounds = new google.maps.LatLngBounds();
+
+      places.forEach((place) => {
+        if (!place.geometry || !place.geometry.location) {
+          console.log("Returned place contains no geometry");
+          return;
+        }
+
+        const icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25),
+        };
+
+        // Create a marker for each place.
+        markers.push(
+          new google.maps.Marker({
+            map,
+            icon,
+            title: place.name,
+            position: place.geometry.location,
+          })
+        );
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
 }
 
 new App();
